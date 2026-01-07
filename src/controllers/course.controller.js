55702096efc8ef96477d3fc616125b3343111be0
Sandesh-1511 <sandeshpatil1511@gmail.com>
2026-01-08 02:20:59 +0530

    Added some endpoints

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -18,6 +18,79 @@
 
 
 
+// const Course = require("../models/course.model");
+
+// exports.createCourse = async (req, res, next) => {
+//   try {
+//     const course = await Course.create(req.body);
+//     res.status(201).json(course);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// exports.getCourses = async (req, res, next) => {
+//   try {
+//     const courses = await Course.find()
+//       .populate("department", "name");
+//     res.json(courses);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// exports.getCourseById = async (req, res, next) => {
+//   try {
+//     const course = await Course.findById(req.params.id)
+//       .populate("department", "name");
+
+//     if (!course) {
+//       return res.status(404).json({ message: "Course not found" });
+//     }
+
+//     res.json(course);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// exports.updateCourse = async (req, res, next) => {
+//   try {
+//     const course = await Course.findByIdAndUpdate(
+//       req.params.id,
+//       req.body,
+//       { new: true }
+//     );
+
+//     if (!course) {
+//       return res.status(404).json({ message: "Course not found" });
+//     }
+
+//     res.json(course);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// exports.deleteCourse = async (req, res, next) => {
+//   try {
+//     const course = await Course.findByIdAndDelete(req.params.id);
+
+//     if (!course) {
+//       return res.status(404).json({ message: "Course not found" });
+//     }
+
+//     res.json({ message: "Course deleted successfully" });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+
+
+
+
+
 const Course = require("../models/course.model");
 
 exports.createCourse = async (req, res, next) => {
@@ -31,8 +104,16 @@ exports.createCourse = async (req, res, next) => {
 
 exports.getCourses = async (req, res, next) => {
   try {
-    const courses = await Course.find()
-      .populate("department", "name");
+    const filter = {};
+
+    // support: /api/courses?departmentId=
+    if (req.query.departmentId) {
+      filter.departmentId = req.query.departmentId;
+    }
+
+    const courses = await Course.find(filter)
+      .populate("departmentId", "name code status");
+
     res.json(courses);
   } catch (err) {
     next(err);
@@ -42,45 +123,13 @@ exports.getCourses = async (req, res, next) => {
 exports.getCourseById = async (req, res, next) => {
   try {
     const course = await Course.findById(req.params.id)
-      .populate("department", "name");
+      .populate("departmentId", "name code status");
 
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
 
     res.json(course);
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.updateCourse = async (req, res, next) => {
-  try {
-    const course = await Course.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-
-    if (!course) {
-      return res.status(404).json({ message: "Course not found" });
-    }
-
-    res.json(course);
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.deleteCourse = async (req, res, next) => {
-  try {
-    const course = await Course.findByIdAndDelete(req.params.id);
-
-    if (!course) {
-      return res.status(404).json({ message: "Course not found" });
-    }
-
-    res.json({ message: "Course deleted successfully" });
   } catch (err) {
     next(err);
   }
--- src/models/course.model.js
+++ src/models/course.model.js
@@ -46,17 +46,50 @@
 
 
 
+// const mongoose = require("mongoose");
+
+// const courseSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     departmentId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Department",
+//       required: true
+//     },
+//     duration: { type: String, required: true },
+//     status: {
+//       type: String,
+//       enum: ["Active", "Inactive"],
+//       default: "Active"
+//     }
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Course", courseSchema);
+
+
+
+
+
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
     departmentId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Department",
       required: true
     },
-    duration: { type: String, required: true },
+    duration: {
+      type: String,
+      required: true
+    },
     status: {
       type: String,
       enum: ["Active", "Inactive"],
