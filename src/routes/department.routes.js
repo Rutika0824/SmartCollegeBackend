82ec3959f7293cd9b1bac267ec95a850e6afe713
Sandesh-1511 <sandeshpatil1511@gmail.com>
2026-01-07 23:57:39 +0530

    Registered first user

--- src/routes/department.routes.js
+++ src/routes/department.routes.js
@@ -1,9 +1,25 @@
+// const router = require("express").Router();
+// const auth = require("../middleware/auth.middleware");
+// const role = require("../middleware/role.middleware");
+// const ctrl = require("../controllers/department.controller");
+
+// router.post("/", auth, role("Admin"), ctrl.create);
+// router.get("/", auth, ctrl.list);
+
+// module.exports = router;
+
+
+
+
 const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
 const role = require("../middleware/role.middleware");
 const ctrl = require("../controllers/department.controller");
 
-router.post("/", auth, role("Admin"), ctrl.create);
-router.get("/", auth, ctrl.list);
+router.post("/", auth, role("Admin"), ctrl.createDepartment);
+router.get("/", auth, ctrl.getDepartments);
+router.get("/:id", auth, ctrl.getDepartmentById);
+router.put("/:id", auth, role("Admin"), ctrl.updateDepartment);
+router.delete("/:id", auth, role("Admin"), ctrl.deleteDepartment);
 
 module.exports = router;
